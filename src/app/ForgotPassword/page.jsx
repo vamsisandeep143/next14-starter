0d07119375c4a626506fc77ad4bf718eee600ae5
sandeep <vamsisandeep.sundara@gmail.com>
2024-03-07 08:16:07 +0530

    initialcommit

---
+++ src/app/ForgotPassword/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ForgotPassword = () => {
+  return (
+    <div>
+    About Page  
+    </div>
+  )
+}
+
+export default ForgotPassword
---
+++ src/app/Login/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+    About Page  
+    </div>
+  )
+}
+
+export default Login
